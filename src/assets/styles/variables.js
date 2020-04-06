7ce1549532fe26b7a2797e354b47a3ceb8682dbe
Pawel Piatek <pawelpiatek98@gmail.com>
2020-04-06 20:36:58 +0200

    end 06.04

--- src/assets/styles/variables.js
+++ src/assets/styles/variables.js
@@ -4,3 +4,4 @@ export const white = "#ffffff";
 export const primaryBlue = "#00a8e0";
 export const red1 = "#e04646";
 export const green1 = "#598c14";
+export const yellow1 = "#ffc719"
--- src/components/mainCountry/mainCountry.js
+++ src/components/mainCountry/mainCountry.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import axios from 'axios';
 import * as variables from '../../assets/styles/variables';
 import ColorSpan from '../../assets/reusable/components/colorSpan';
-import {LineChart, Line, CartesianGrid, XAxis, YAxis, ResponsiveContainer} from 'recharts';
+import {LineChart, Line, CartesianGrid, XAxis, YAxis, ResponsiveContainer, Tooltip, BarChart, Bar, Cell} from 'recharts';
 
 const MainCountryWrapper = styled.div`
 
@@ -22,6 +22,10 @@ const Cases = styled.div`
 margin: 6rem 0 0 3rem;
 `;
 
+const RateContainer = styled.div`
+
+`;
+
 const Paragraph = styled.p`
 font-size: 1.6rem;
 
@@ -38,18 +42,20 @@ margin-top: 10rem;
 `;
 
 const ChartHeader = styled.h1`
-font-weight: 500;
+font-weight: 400;
 `;
 
 const MainCountry = ({data}) => {
 
     const [cases, setCases] = useState([]);
+    const [casesDaily, setCasesDaily] = useState([]);
 
     useEffect(() => {
         axios.get(`https://pomber.github.io/covid19/timeseries.json`)
             .then(response => {
                 const newArr = [];
                 const baseArr = response.data['Poland'];
+                const casesEveryDay=[];
                 baseArr
                     .filter(el => {
                         const date = parseInt(el.date.slice(5, 6))
@@ -59,15 +65,35 @@ const MainCountry = ({data}) => {
 
                     })
                     .map(el => {
-                        console.log(el.date.slice(5, 6))
                         newArr.push({
                             name: el.date,
-                            uv: el.confirmed,
-                            amt: 2400
-
+                            Polska: el.confirmed,
                         })
+                    });
+
+                    for(let i=0;i<baseArr.length; i++){
+                        if(i>0){
+                            casesEveryDay.push({
+                                name: baseArr[i].date,
+                                Polska: baseArr[i].confirmed - baseArr[i-1].confirmed
+                            })
+                        }else{
+                            casesEveryDay.push({
+                                name: baseArr[i].date,
+                                Polska: baseArr[i].confirmed
+                            })
+                        }
+
+                    }
+
+                    const casesFromMarch = casesEveryDay.filter(el=>{
+                        const date = parseInt(el.name.slice(5, 6))
+                        if (date > 2) {
+                            return el
+                        }
                     })
 
+                setCasesDaily(casesFromMarch);
                 return newArr
             })
             .then(arr => {
@@ -75,12 +101,12 @@ const MainCountry = ({data}) => {
             })
     }, [])
 
-
+    const rate = parseInt(data.deaths)/parseInt(data.confirmed)
     return (
         <MainCountryWrapper>
             <Intro>
 
-                <MainCountryHeader>Przypadki w Polsce</MainCountryHeader>
+                <MainCountryHeader>Statystyki w Polsce</MainCountryHeader>
                 <Cases>
                     <Paragraph><span>Potwierdzone przypadki: </span><ColorSpan
                         color={variables.primaryBlue}>{data.confirmed}</ColorSpan></Paragraph>
@@ -88,17 +114,31 @@ const MainCountry = ({data}) => {
                         color={variables.red1}>{data.deaths}</ColorSpan></Paragraph>
                     <Paragraph><span>Wyleczone przypadki: </span><ColorSpan
                         color={variables.green1}>{data.recovered}</ColorSpan></Paragraph>
+                    <Paragraph><span>Wsółczynnik śmiertelności: </span>
+                        <ColorSpan color={variables.yellow1}>{rate.toFixed(4)*10}%</ColorSpan>
+                    </Paragraph>
                 </Cases>
             </Intro>
             <ChartsWrapper>
                 <ChartHeader>Liczba zachorowań w Polsce w kolejnych dniach</ChartHeader>
                 <ResponsiveContainer width="100%" height={400}>
                     <LineChart  data={cases} margin={{top: 5, right: 20, bottom: 5, left: 0}}>
-                        <Line type="monotone" dataKey="uv" stroke="#8884d8"/>
+                        <Line type="monotone" dataKey="Polska" stroke="#8884d8"/>
                         <CartesianGrid stroke="#ccc" strokeDasharray="5 5"/>
                         <XAxis dataKey="name"/>
                         <YAxis/>
+                        <Tooltip/>
                     </LineChart>
+                </ResponsiveContainer>
+                <ChartHeader>Dzienna liczba zachorowań w Polsce</ChartHeader>
+                <ResponsiveContainer width="100%" height={400}>
+                    <BarChart  data={casesDaily} margin={{top: 5, right: 20, bottom: 5, left: 0}}>
+                        <Bar type="monotone" dataKey="Polska" stroke="#8884d8"/>
+                        <CartesianGrid stroke="#ccc" strokeDasharray="5 5"/>
+                        <XAxis dataKey="name"/>
+                        <YAxis/>
+                        <Tooltip/>
+                    </BarChart>
                 </ResponsiveContainer>
 
             </ChartsWrapper>
